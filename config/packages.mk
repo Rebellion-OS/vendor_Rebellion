2aae7d719c52d92dd4d5ccda8ae9e08a7494f65d
Alif Fathur <darkaosp@gmail.com>
2019-06-26 02:04:26 +0000

    vendor: Add additional packages

--- config/packages.mk
+++ config/packages.mk
@@ -7,8 +7,15 @@ PRODUCT_PACKAGES += \
     Dialer \
     ExactCalculator \
     Gallery2 \
-    Launcher3QuickStep \
+    LatinIME \
+    Lauwnchair \
+    LawnConf \
     Messaging \
     OmniStyle \
     Phonograph \
     Recorder
+
+# Lawnchair Permissions
+PRODUCT_COPY_FILES += \
+    vendor/rebellion/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/rebellion/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
